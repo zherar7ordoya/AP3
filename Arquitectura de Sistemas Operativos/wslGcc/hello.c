414b79c51115c5d0a9a91cef129538b0353db99e
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-08-20 12:39:22 -0300

    Darwin

---
+++ Arquitectura de Sistemas Operativos/wslGcc/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+   printf("\n");
+   printf("\t\t\t+----------------+\n");
+   printf("\t\t\t| ¡Hola Gerardo! |\n");
+   printf("\t\t\t+----------------+\n");
+   printf("\n");
+   return 0;
+}
