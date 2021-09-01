afdc9ccfc648f74c3004242ba5201574b8f78578
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-09-01 19:55:20 -0300

    WSL

---
+++ Arquitectura de Sistemas Operativos/EjemplosC/hello.c
@@ -0,0 +1,12 @@
+/**
+ * COMPILAR: gcc hello.c -o hello
+ * EJECUTAR: ./hello (Ubuntu) hello (Windows)
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\nHola Gerardo, comencemos...\n\n");
+    return 0;
+}
