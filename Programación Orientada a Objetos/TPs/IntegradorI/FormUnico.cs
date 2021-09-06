916ad018f6ba93287cda6b77568eddf164f8b218
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-09-06 19:53:22 -0300

    .

---
+++ Programación Orientada a Objetos/TPs/IntegradorI/FormUnico.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntegradorI
+{
+    public partial class FormUnico : Form
+    {
+        public FormUnico()
+        {
+            InitializeComponent();
+        }
+
+        private void FormUnico_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            // TEMPORAL
+            dgvPersonas.SelectionChanged -= new EventHandler(this.dgvPersonas_SelectionChanged);
+            dgvPersonas.Rows.Add("Gerardo");
+            dgvPersonas.Rows.Add("Rodolfo");
+            dgvPersonas.Rows.Add("Tordoya");
+            dgvPersonas.ClearSelection();
+            dgvPersonas.SelectionChanged += new EventHandler(this.dgvPersonas_SelectionChanged);
+            dgvAutos.Rows.Add("UNO");
+            dgvAutos.Rows.Add("DOS");
+            dgvAutos.Rows.Add("TRES");
+        }
+        #region PERSONAS
+        private void btnAltaPersona_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnBajaPersona_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnModificacionPersona_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void dgvPersonas_SelectionChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Soy SelectionChanged de PERSONAS");
+        }
+        #endregion
+
+        #region AUTOS
+        private void btnAltaAuto_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnBajaAuto_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnModificacionAuto_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void dgvAutos_SelectionChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Soy SelectionChanged de AUTOS");
+        }
+        #endregion
+
+        #region COMUNES
+        private void btnAsignar_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
