d55be80bfc60888b3010516e255a81a25f979105
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-09-13 12:55:55 -0300

    .

---
+++ Programación Orientada a Objetos/VC 05 25 Ejemplos/Ej0008/Ej0008/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej0008
+{
+    internal class Test
+    {
+        public int X = 10;
+    }
+}
