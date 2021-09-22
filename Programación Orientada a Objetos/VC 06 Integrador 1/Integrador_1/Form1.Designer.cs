e8b36f71ca2a5c33b8f31f8abe31a415ac914bd4
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-09-22 19:37:54 -0300

    .

---
+++ Programación Orientada a Objetos/VC 06 Integrador 1/Integrador_1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace Integrador_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button7 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 28);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(416, 204);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 246);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 246);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 21);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Borrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(243, 246);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 21);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Modificar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(759, 246);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 21);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Modificar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(655, 246);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 21);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Borrar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(554, 246);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 21);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Agregar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(554, 28);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 82;
+            this.dataGridView2.RowTemplate.Height = 33;
+            this.dataGridView2.Size = new System.Drawing.Size(661, 204);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(458, 105);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(89, 21);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "<< Asignar";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(38, 299);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowHeadersWidth = 82;
+            this.dataGridView3.RowTemplate.Height = 33;
+            this.dataGridView3.Size = new System.Drawing.Size(661, 200);
+            this.dataGridView3.TabIndex = 9;
+            this.dataGridView3.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView3_RowsAdded);
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.AllowUserToAddRows = false;
+            this.dataGridView4.AllowUserToDeleteRows = false;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(38, 545);
+            this.dataGridView4.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.ReadOnly = true;
+            this.dataGridView4.RowHeadersWidth = 82;
+            this.dataGridView4.RowTemplate.Height = 33;
+            this.dataGridView4.Size = new System.Drawing.Size(1177, 233);
+            this.dataGridView4.TabIndex = 10;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(703, 299);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(89, 21);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "<< Quitar";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1402, 817);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Programación Orientada a Objetos/VC 06 Integrador 1/Integrador_1/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Integrador_1
+{
+    public partial class Form1 : Form
+    {
+        Concesionario _c;
+        VistaAuto _va;
+        public Form1()
+        {
+            InitializeComponent();
+            _c = new Concesionario();
+            _va = new VistaAuto();
+        }
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.MultiSelect = false;
+            dataGridView2.MultiSelect = false;
+            dataGridView3.MultiSelect = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView3.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            RefrescaGrilla(dataGridView1, _c.RetornaListaPersonas());
+            RefrescaGrilla(dataGridView2, _c.RetornaListaAutos());
+            RefrescaGrilla(dataGridView3, _c.RetornaListaAutos());
+        }
+
+        void RefrescaGrilla(DataGridView pDGV, object pObject)
+        {
+            pDGV.DataSource = null;pDGV.DataSource = pObject;
+        }
+        //todo ABM Autos
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+               //todo validar DNI
+                string _auxdni = Interaction.InputBox("DNI: ");
+                _c.AgregarPersona(new Persona(_auxdni, Interaction.InputBox("Nombre: "), Interaction.InputBox("Apellido: ")));
+                RefrescaGrilla(dataGridView1,_c.RetornaListaPersonas());
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+               // Verifica que haya datods para borrar
+                if(dataGridView1.Rows.Count>0)
+                {
+                    // Se le pasa a BorrarPpersona del concesionario La persona seleccionado en el grilla 1
+                    _c.BorrarPersona(dataGridView1.SelectedRows[0].DataBoundItem as Persona);
+                    RefrescaGrilla(dataGridView1,_c.RetornaListaPersonas());
+                }
+               else
+                { throw new Exception("No hay nada que borrar !!!"); }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Verifica si hay algo para modificar
+                if(dataGridView1.Rows.Count>0)
+                {
+                    // Obtiene la persona seleccionada en la grilla 1
+                    Persona _auxPersona = dataGridView1.SelectedRows[0].DataBoundItem as Persona;
+                    // Si la persona en distinta de nulo se procede a la modificación
+                    if(_auxPersona!=null)
+                    {
+                        // Se modifica el nombre
+                        _auxPersona.Nombre = Interaction.InputBox("Nombre: ","Modificando Persona ...", _auxPersona.Nombre);
+                        // Se modifica el apellido
+                        _auxPersona.Apellido = Interaction.InputBox("Apellido: ", "Modificando Persona ...", _auxPersona.Apellido);
+                        // Se envía a ModificarPersona del concesionario la persona seleccionada en la grilla 
+                        // con el estado actualizado
+                        _c.ModificarPersona(_auxPersona);
+                        RefrescaGrilla(dataGridView1,_c.RetornaListaPersonas());
+                        // Opción con LinQ
+                        // RefrescaGrilla(dataGridView4, _va.VistaAutos(_c.RetornaListaAutos());
+
+                        // Opción con Clase Vista
+                        RefrescaGrilla(dataGridView4, _va.RetornaVistaAuto(_c.RetornaListaAutos()));
+                    }
+                    else { throw new Exception("La persona que intenta modificar en null"); }
+                }
+                else { throw new Exception("No existe persona para modificar !!!"); }
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //todo validar PATENTE
+                string _auxpatente = Interaction.InputBox("Patente: ");
+                _c.AgregarAuto(new Auto(_auxpatente, Interaction.InputBox("Marca: "), Interaction.InputBox("Modelo: "), Interaction.InputBox("Año: "),decimal.Parse(Interaction.InputBox("Precio: "))));
+                RefrescaGrilla(dataGridView2, _c.RetornaListaAutos());
+                // Opción con LinQ
+                // RefrescaGrilla(dataGridView4, _va.VistaAutos(_c.RetornaListaAutos());
+
+                // Opción con Clase Vista
+                RefrescaGrilla(dataGridView4, _va.RetornaVistaAuto(_c.RetornaListaAutos()));
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Se controla que el la grilla 2 haya algo para borrar
+                if (dataGridView2.Rows.Count > 0)
+                {
+
+                    // Se envía el Auto seleccionado en la grilla 2 a BorrarAuto del concesionario
+                    _c.BorrarAuto(dataGridView2.SelectedRows[0].DataBoundItem as Auto);
+                    RefrescaGrilla(dataGridView2, _c.RetornaListaAutos());
+                    // Opción con LinQ
+                    // RefrescaGrilla(dataGridView4, _va.VistaAutos(_c.RetornaListaAutos()));
+
+                    // Opción con Clase Vista
+                    RefrescaGrilla(dataGridView4, _va.RetornaVistaAuto(_c.RetornaListaAutos()));
+                }
+                else
+                { throw new Exception("No hay nada que borrar !!!"); }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Verifica si hay algún auto que modificar en la grilla 2
+                if (dataGridView2.Rows.Count > 0)
+                {
+                    // Se obtiene el auto seleccionado en la grilla 2
+                    Auto _auxAuto = dataGridView2.SelectedRows[0].DataBoundItem as Auto;
+                    if (_auxAuto != null)
+                    {
+                       // Se actualiza el estado del auto
+                        _auxAuto.Marca = Interaction.InputBox("Marca: ", "Modificando Auto ...", _auxAuto.Marca);
+                        _auxAuto.Modelo = Interaction.InputBox("Modelo: ", "Modificando Auto ...", _auxAuto.Modelo);
+                        _auxAuto.Axo = Interaction.InputBox("Año: ", "Modificando Auto ...", _auxAuto.Axo);
+                        _auxAuto.Precio = decimal.Parse(Interaction.InputBox("Precio: ", "Modificando Auto ...", _auxAuto.Precio.ToString()));
+                        // Se llama a ModificarAuto del concesionario
+                        _c.ModificarAuto(_auxAuto);
+                        RefrescaGrilla(dataGridView2, _c.RetornaListaAutos());
+                        RefrescaGrilla(dataGridView3, _c.AutosDeUnaPersona(dataGridView1.SelectedRows[0].DataBoundItem as Persona));
+
+                        // Opción con LinQ
+                        // RefrescaGrilla(dataGridView4, _va.VistaAutos(_c.RetornaListaAutos());
+
+                        // Opción con Clase Vista
+                        RefrescaGrilla(dataGridView4, _va.RetornaVistaAuto(_c.RetornaListaAutos()));
+                        ActualizaPrecioTotal();
+                    }
+                    else { throw new Exception("El auto que intenta modificar en null"); }
+                }
+                else { throw new Exception("No existe auto para modificar !!!"); }
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Se envía a Asignar auto del concesionario la Persona seleccionada en la grilla 1
+                // y el auto seleccionado en la grilla 2
+                _c.AsignarAuto(dataGridView1.SelectedRows[0].DataBoundItem as Persona, dataGridView2.SelectedRows[0].DataBoundItem as Auto);
+                RefrescaGrilla(dataGridView3, _c.AutosDeUnaPersona(dataGridView1.SelectedRows[0].DataBoundItem as Persona));
+               
+                // Opción con LinQ
+                 RefrescaGrilla(dataGridView4, _va.VistaAutos(_c.RetornaListaAutos()));
+                
+                // Opción con Clase Vista
+               // RefrescaGrilla(dataGridView4,_va.RetornaVistaAuto(_c.RetornaListaAutos()));
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+               
+                RefrescaGrilla(dataGridView3, _c.AutosDeUnaPersona(dataGridView1.SelectedRows[0].DataBoundItem as Persona));
+                ActualizaPrecioTotal();
+                
+            }
+            catch (Exception) { }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Se envia a QuitarAutoAsignado del concesionario:
+                // 1. La persona seleccionada en la grilla 1 (Esto también se puede ubicar tomando el auto
+                // seleccionado en la grilla 3 y consultandole por su dueño). 
+                // 2. El auto seleccionado en la grilla 3.
+                _c.QuitarAutoAsignado(dataGridView1.SelectedRows[0].DataBoundItem as Persona, dataGridView3.SelectedRows[0].DataBoundItem as Auto);
+                RefrescaGrilla(dataGridView3, _c.AutosDeUnaPersona(dataGridView1.SelectedRows[0].DataBoundItem as Persona));
+
+                // Opción con LinQ
+                // RefrescaGrilla(dataGridView4, _va.VistaAutos(_c.RetornaListaAutos());
+
+                // Opción con Clase Vista
+                RefrescaGrilla(dataGridView4, _va.RetornaVistaAuto(_c.RetornaListaAutos()));
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+        private void ActualizaPrecioTotal()
+        {
+            decimal _total = 0;
+            List<Auto> _la = _c.AutosDeUnaPersona(dataGridView1.SelectedRows[0].DataBoundItem as Persona);
+            if (_la.Count > 0)
+            {
+                foreach (Auto _a in _la)
+                {
+                    _total += _a.Precio;
+                }
+            }
+            label1.Text = _total.ToString();
+        }
+
+        private void dataGridView3_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            ActualizaPrecioTotal();
+        }
+    }
+}
