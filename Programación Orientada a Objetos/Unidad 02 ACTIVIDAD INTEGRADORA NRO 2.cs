2ff89a948555ff1df458f58481b275a33f9a0bb2
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-10-06 13:03:50 -0300

    .

--- Programación Orientada a Objetos/Unidad 02 ACTIVIDAD INTEGRADORA NRO 2.cs
+++ Programación Orientada a Objetos/Unidad 02 ACTIVIDAD INTEGRADORA NRO 2.cs
@@ -1,67 +1,43 @@
- public class Persona
-
-  {
-
-
-
+public class Persona
+{
     public void Compra(Accion _accion, int _cantidad)
-
     {
-
-      _accion.CambioCotizacion += M1;
-
+        _accion.CambioCotizacion = M1;
     }
-
-
-
     private void M1(object sender, CambioCotizacionEventArg e)
-
     {
-
-      //e.CotizacionActual;
-
+        //e.CotizacionActual;
     }
-
-  }
-
-
-
-  public class Accion
-
-  {
-
-    public event EventHandler<CambioCotizacionEventArg> CambioCotizacion;
-
+}
+public class Accion
+{
+    public event EventHandler < CambioCotizacionEventArg > CambioCotizacion;
     decimal _cotActual;
-
-    public decimal CotActual 
-
+    public decimal CotActual
     {
-
-      get { return _cotActual; }
-
-      set { _cotActual = value; CambioCotizacion?.Invoke(this,new CambioCotizacionEventArg(this)); } 
-
+        get
+        {
+            return _cotActual;
+        }
+        set
+        {
+            _cotActual = value;
+            CambioCotizacion?.Invoke(this, new CambioCotizacionEventArg(this));
+        }
     }
-
-
-
-  }
-
-  public class CambioCotizacionEventArg : EventArgs
-
-  {
-
+}
+public class CambioCotizacionEventArg: EventArgs
+{
     Accion _accion = null;
-
-    public CambioCotizacionEventArg(Accion pAccion) { _accion = pAccion; }
-
-
-
-    public decimal CotizacionActual { get { return _accion.CotActual; } }
-
-
-
-
-
-  }
+    public CambioCotizacionEventArg(Accion pAccion)
+    {
+        _accion = pAccion;
+    }
+    public decimal CotizacionActual
+    {
+        get
+        {
+            return _accion.CotActual;
+        }
+    }
+}
