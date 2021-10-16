b27531f9c47ef564cb4d24d2a2b813474e759f92
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-10-16 12:42:35 -0300

    .

---
+++ 6erardo7ordoya/NiCosio/InterfazCallBack/CSink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace InterfazCallBack
+{
+    /// <summary>
+    /// Handlers de los eventos definidos en la interfaz.
+    /// In computing, a sink, event sink or data sink is a class or 
+    /// function designed to receive incoming events from another object or 
+    /// function. This is commonly implemented in C++ as callbacks. 
+    /// Other object-oriented languages, such as Java and C#, have built-in 
+    /// support for sinks by allowing events to be fired to delegate functions.
+    /// </summary>
+    class CSink : IEvento
+    {
+        /// <summary>
+        /// Código a ejecutar cuando suceda el evento.
+        /// </summary>
+        /// <param name="pKilos"></param>
+        public void EReservasBajas(int pKilos)
+        {
+            ForegroundColor = ConsoleColor.Yellow;
+            WriteLine("--> Reservas bajas de alimentos");
+            WriteLine($"--> Quedan { pKilos }");
+        }
+    }
+}
---
+++ 6erardo7ordoya/NiCosio/InterfazCallBack/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace InterfazCallBack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CRefrigerador refrigerador = new CRefrigerador(50, -20);
+            Random aleatorio = new Random();
+            CSink sink1 = new CSink();
+
+            /* Ya tenemos el refrigerador y ya tenemos el sink. Lo único que
+             * nos falta es llevar a cabo esa unión entre ambos de tal forma
+             * que se puedan comunicar entre ellos. Esto se hará mediante el
+             * método AgregarSink().
+             * Al pasar el objeto "sink1", este ya queda en la lista de los
+             * sinks que tiene la clase CRefrigerador y cuando suceda el even-
+             * to, su método (el de la lista) va a ser invocado.
+             */
+            refrigerador.AgregarSink(sink1);
+
+            while (refrigerador.Kilos > 0)
+            {
+                refrigerador.Trabajar(aleatorio.Next(1, 5));
+            }
+            ReadKey();
+        }
+    }
+}
