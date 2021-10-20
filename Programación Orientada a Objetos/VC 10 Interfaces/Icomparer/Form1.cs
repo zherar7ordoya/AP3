6c5a6faa0e17824792747c8ef249eb96856b2466
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-10-20 19:55:14 -0300

    .

---
+++ Programación Orientada a Objetos/VC 10 Interfaces/Icomparer/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Icomparer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Persona p1;
+        Persona p2;
+        Persona p3;
+        List<Persona> l;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            p1 = new Persona("Juan", "Garcia", 22);
+            p2 = new Persona("Ana", "Perez", 35);
+            p3 = new Persona("Cecilia", "Alvarez", 29);
+            l = new List<Persona>() { p1, p2, p3 };
+
+            listBox1.Items.AddRange(l.ToArray());
+            l.Sort(new Persona.NombreASC()); listBox2.Items.AddRange(l.ToArray());
+            l.Sort(new Persona.NombreDESC()); listBox3.Items.AddRange(l.ToArray());
+            l.Sort(new Persona.ApellidoASC()); listBox4.Items.AddRange(l.ToArray());
+            l.Sort(new Persona.ApellidoDESC()); listBox5.Items.AddRange(l.ToArray());
+            l.Sort(new Persona.EdadASC()); listBox6.Items.AddRange(l.ToArray());
+            l.Sort(new Persona.EdadDESC()); listBox7.Items.AddRange(l.ToArray());
+
+            Array.Sort()
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Persona
+    {
+      
+        public Persona(string pNombre, string pApellido, int pEdad) { Nombre = pNombre; Apellido = pApellido; Edad = pEdad; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public override string ToString()
+        {
+            return $"{Nombre} {Apellido} {Edad}";
+        }
+        public class NombreASC : IComparer<Persona>
+        {
+            public int Compare(Persona x, Persona y)
+            {
+                return x.Nombre.CompareTo(y.Nombre);
+            }
+        }
+        public class NombreDESC : IComparer<Persona>
+        {
+            public int Compare(Persona x, Persona y)
+            {
+                return (x.Nombre.CompareTo(y.Nombre))*-1;
+            }
+        }
+        public class ApellidoASC : IComparer<Persona>
+        {
+            public int Compare(Persona x, Persona y)
+            {
+                return x.Apellido.CompareTo(y.Apellido);
+            }
+        }
+        public class ApellidoDESC : IComparer<Persona>
+        {
+            public int Compare(Persona x, Persona y)
+            {
+                return (x.Apellido.CompareTo(y.Apellido)) * -1;
+            }
+        }
+        public class EdadASC : IComparer<Persona>
+        {
+            public int Compare(Persona x, Persona y)
+            {
+                int rdo = 0;
+                if (x.Edad < y.Edad) { rdo = -1; };
+                if (x.Edad > y.Edad) { rdo = 1; };
+                return rdo; 
+            }
+        }
+        public class EdadDESC : IComparer<Persona>
+        {
+            public int Compare(Persona x, Persona y)
+            {
+                int rdo = 0;
+                if (x.Edad < y.Edad) { rdo = -1; };
+                if (x.Edad > y.Edad) { rdo = 1; };
+                return rdo * -1;
+            }
+        }
+    }
+      
+
+   
+}
---
+++ Programación Orientada a Objetos/VC 10 Interfaces/Interfaces/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Interfaces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(284, 236);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 29;
+            this.listBox2.Location = new System.Drawing.Point(351, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(284, 236);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 29;
+            this.listBox3.Location = new System.Drawing.Point(0, 252);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(635, 236);
+            this.listBox3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
