c7fafc61ce6962444ad8ac4c2608dc561b1a063e
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-11-02 12:24:13 -0300

    .

--- A dog chasing cars/DanClark/Orientador2/Ej0001/Ej0001/Form1.cs
+++ A dog chasing cars/DanClark/Orientador2/Ej0001/Ej0001/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.VisualBasic;
 
@@ -41,7 +34,8 @@ namespace Ej0001
             get { return Vnombre; }
             set
             {
-                this.Vnombre = value; CambioEnNombre(this, null);
+                this.Vnombre = value;
+                CambioEnNombre(this, null);
              }
         }
     }
