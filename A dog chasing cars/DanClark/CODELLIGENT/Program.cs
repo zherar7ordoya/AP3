1f53d7ed2914498d3b88a8d6d8be8912bacf633e
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-11-02 19:39:37 -0300

    .

---
+++ A dog chasing cars/DanClark/CODELLIGENT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODELLIGENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ A dog chasing cars/DanClark/Delegados/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace Delegados
+{
+    public delegate void Delegado();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegado delegado = Método;
+            delegado();
+            ReadKey();
+        }
+        public static void Método() { Write("Soy el método."); }
+    }
+}
