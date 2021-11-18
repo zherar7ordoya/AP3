d375ee27f63c75d3b216b4537896078ede7c53f3
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-11-18 20:08:30 -0300

    .

--- A dog chasing cars/DanClark/CODELLIGENT/SAS/Form1.cs
+++ A dog chasing cars/DanClark/CODELLIGENT/SAS/Form1.cs
@@ -14,9 +14,8 @@ namespace SAS
     public partial class CFormulario : Form
     {
         #region // *----------------------------------------------=> Formulario
+        // Default
         public CFormulario() { InitializeComponent(); }
-
-        string Usuario = String.Empty;
         private void InformaExcepcion(Control pControl, string pMensaje)
         {
             try
@@ -37,91 +36,15 @@ namespace SAS
             catch (Exception error)
             { InformaExcepcion(LabelInformacion, error.Message); }
         }
-        private void IniciaFormulario()
-        {
-            try
-            {
-                Usuario = Interaction.InputBox
-                    (
-                    "Ingrese su nombre:",
-                    "Usuario",
-                    "Gerardo Tordoya"
-                    );
-                this.CenterToScreen();
-                this.FormBorderStyle = FormBorderStyle.FixedSingle;
-                this.Text += $" ({Usuario})";
-                this.MaximizeBox = false;
-                this.MinimizeBox = false;
-            }
-            catch (Exception error)
-            { InformaExcepcion(LabelInformacion, error.Message); }
-        }
-        private void CFormulario_Load(object sender, EventArgs e)
-        {
-            try
-            { 
-                
-                
-                IniciaFormulario();
 
+        // Variables globales
+        string Usuario = String.Empty;
 
-                foreach (Control gbox in this.Controls)
-                {
-                    if (gbox is GroupBox)
-                    {
-                        foreach (Control tbox in gbox.Controls)
-                        {
-                            if (tbox is TextBox)
-                            {
-                                //tbox.TextChanged += new EventHandler(TextBox_OnTextChanged);
-                                tbox.GotFocus += new EventHandler(TboxGanaFoco);
-                                tbox.LostFocus += new EventHandler(TboxPierdeFoco);
-                            }
-                        }
-                    }
-                }
-
-                //foreach (Control gbox in this.Controls)
-                //{
-                //    if (gbox is GroupBox)
-                //    {
-                //        foreach (TextBox tbox in Controls.OfType<TextBox>())
-                //        {
-                //            //tbox.TextChanged += new EventHandler(TextBox_OnTextChanged);
-                //            tbox.GotFocus += new EventHandler(TboxGanaFoco);
-                //            tbox.LostFocus += new EventHandler(TboxPierdeFoco);
-                //        }
-                //    }
-                //}
-
-
-                //foreach (Control x in this.Controls)
-                //{
-                //    if (x is TextBox)
-                //    {
-                //        ((TextBox)x).Text = String.Empty;
-                //    }
-                //}
-
-
-
-
-
-            }
-            catch (Exception error)
-            { InformaExcepcion(LabelInformacion, error.Message); }
-
-
-
-        
-
-        }
-        //void TextBox_OnTextChanged(object sender, EventArgs e)
-        //{
-        //    TextBox tbox = (TextBox)sender;
-        //    tbox.Tag = "CHANGED"; // or whatever
-        //}
-        public void TboxGanaFoco(object sender, EventArgs e)
+        // UI (Interfaz de Usuario) Eventos
+        private void TimerReloj_Tick(object sender, EventArgs e)
+        { this.LabelSesion.Text = DateTime.Now.ToString(); }
+        // TBOX
+        public void TboxAdquiereFoco(object sender, EventArgs e)
         {
             TextBox tbox = (TextBox)sender;
             if (tbox.Text == tbox.Tag.ToString())
@@ -133,9 +56,138 @@ namespace SAS
             if (String.IsNullOrWhiteSpace(tbox.Text))
             { tbox.Text = tbox.Tag.ToString(); }
         }
+        // MTBOX
+        private void MTboxLegajo_Enter(object sender, EventArgs e)
+        { LabelInformacion.Text = "FORMATO DE LEGAJO\n\n4 dígitos"; }
+        private void MTboxCodigo_Enter(object sender, EventArgs e)
+        { LabelInformacion.Text = "FORMATO DE CÓDIGO\n\n4 dígitos y  4 letras"; }
+        private void MTboxLegajo_MouseHover(object sender, EventArgs e)
+        { LabelInformacion.Text = "Legajo"; }
+        private void MTboxCodigo_MouseHover(object sender, EventArgs e)
+        { LabelInformacion.Text = "Código"; }
+        // DTP
+        private void DtpFechaOtorgamiento_MouseHover(object sender, EventArgs e)
+        { LabelInformacion.Text = "Fecha de Otorgamiento"; }
+        private void CalFechaCancelacion_MouseHover(object sender, EventArgs e)
+        { LabelInformacion.Text = "Fecha de Cancelación"; }
+        private void DtpFechaOtorgamiento_Enter(object sender, EventArgs e)
+        { LabelInformacion.Text = "Fecha de Otorgamiento"; }
+        private void CalFechaCancelacion_Enter(object sender, EventArgs e)
+        { LabelInformacion.Text = "Fecha de Cancelación"; }
 
+        // Llamadas internas
+        private void IniciaFormulario()
+        {
+            try
+            {
+                while (true)
+                {
+                    if (!String.IsNullOrWhiteSpace(Usuario)) { break; }
+                    Usuario = Interaction.InputBox
+                        (
+                        "Ingrese su nombre:",
+                        "Usuario",
+                        "Gerardo Tordoya"
+                        );
+                    if (String.IsNullOrWhiteSpace(Usuario)) { this.Close(); }
+                }
+                this.CenterToScreen();
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                this.Text += $" ({Usuario})";
+                this.MaximizeBox = false;
+                this.MinimizeBox = false;
+            }
+            catch (Exception error)
+            { InformaExcepcion(LabelInformacion, error.Message); }
+        }
+        private void SimulaPlaceholder()
+        {
+            try
+            {
+                foreach (Control x in this.Controls)
+                {
+                    if (x is GroupBox)
+                    {
+                        foreach (Control box in x.Controls)
+                        {
+                            if (box is TextBox)
+                            {
+                                box.GotFocus += new EventHandler(TboxAdquiereFoco);
+                                box.LostFocus += new EventHandler(TboxPierdeFoco);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            { InformaExcepcion(LabelInformacion, error.Message); }
+        }
 
+        // Carga
+        private void CFormulario_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                IniciaFormulario();
+                SimulaPlaceholder();
+                TEMPORAL();
+            }
+            catch (Exception error)
+            { InformaExcepcion(LabelInformacion, error.Message); }
+        }
+        private void TEMPORAL()
+        {
+            DgvAdelantosPorEmpleado.Rows.Add(new object[] {
+                    "Dato 1",
+                    "Dato 2",
+                    "Dato 3",
+                    "Dato 4",
+                    "Dato 5",
+                    "Dato 6",
+                    "Dato 7"
+                });
+            DgvAdelantosPorEmpleado.Rows.Add(new object[] {
+                    "Dato 11",
+                    "Dato 12",
+                    "Dato 13",
+                    "Dato 14",
+                    "Dato 15",
+                    "Dato 16",
+                    "Dato 17"
+                });
+            DgvTodosLosAdelantos.Rows.Add(new object[] {
+                    "Dato 1",
+                    "Dato 2",
+                    "Dato 3",
+                    "Dato 4",
+                    "Dato 5",
+                    "Dato 6",
+                    "Dato 7"
+                });
+            DgvTodosLosAdelantos.Rows.Add(new object[] {
+                    "Dato 11",
+                    "Dato 12",
+                    "Dato 13",
+                    "Dato 14",
+                    "Dato 15",
+                    "Dato 16",
+                    "Dato 17"
+                });
+            DgvTodosLosEmpleados.Rows.Add(new object[] {
+                    "Dato 1",
+                    "Dato 2",
+                    "Dato 3",
+                    "Dato 4"
+                });
+            DgvTodosLosEmpleados.Rows.Add(new object[] {
+                    "Dato 11",
+                    "Dato 12",
+                    "Dato 13",
+                    "Dato 14"
+                });
+        }
 
+        // Descarga
         private void CFormulario_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (MessageBox.Show
@@ -156,17 +208,50 @@ namespace SAS
             }
             return base.ProcessCmdKey(ref msg, keyData);
         }
-        private void TimerReloj_Tick(object sender, EventArgs e)
-        { this.LabelSesion.Text = DateTime.Now.ToString(); }
-        private void DtpFechaCancelacion_MouseHover(object sender, EventArgs e)
-        { LabelInformacion.Text = "Fecha de Cancelación"; }
-        private void DtpFechaOtorgamiento_MouseHover(object sender, EventArgs e)
-        { LabelInformacion.Text = "Fecha de Otorgamiento"; }
         #endregion
 
-        private void button9_Click(object sender, EventArgs e)
+        #region // *--------------------------------------=> Eventos Operativos
+        private void DgvTodosLosEmpleados_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            this.Controls.OfType<TextBox>().ToList().ForEach(t => t.Text = string.Empty);
+            CmdG1Agregar.Enabled   = true;
+            CmdG1Eliminar.Enabled  = false;
+            CmdG1Modificar.Enabled = false;
         }
+        private void DgvTodosLosEmpleados_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            CmdG1Agregar.Enabled = false;
+            CmdG1Eliminar.Enabled = true;
+            CmdG1Modificar.Enabled = true;
+
+            // PENDIENTE: COPIA DE LOS VALORES DEL DGV A LOS CONTROLES.
+        }
+        private void DgvTodosLosAdelantos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            CmdG2Agregar.Enabled = true;
+            CmdG2Eliminar.Enabled = false;
+            CmdG2Modificar.Enabled = false;
+            /* Hay un evento  de habilitación que falta cuando
+             * se puede cobrar o no.
+             * Depende de que el objeto tenga o no ya fecha de cancelación.
+             * Si tiene, no se habilitan los controles del módulo de pago.
+             * Si no tiene, sí, se habilitan.
+             */
+        }
+        private void DgvTodosLosAdelantos_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            CmdG2Agregar.Enabled = false;
+            CmdG2Eliminar.Enabled = true;
+            CmdG2Modificar.Enabled = true;
+
+            // PENDIENTE: COPIA DE LOS VALORES DEL DGV A LOS CONTROLES.
+
+            /* Hay un evento  de habilitación que falta cuando
+             * se puede cobrar o no.
+             * Depende de que el objeto tenga o no ya fecha de cancelación.
+             * Si tiene, no se habilitan los controles del módulo de pago.
+             * Si no tiene, sí, se habilitan.
+             */
+        }
+        #endregion
     }
 }
