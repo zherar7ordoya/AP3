7428896c3b866c2be06a8352feff574eea16b522
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-12-01 13:03:40 -0300

    .

--- (I'm like a dog chasing cars)/2.RobertMartin&MicahMartin/ModelViewPresenter/1erAcercamiento/Views/IRectangulo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ModelViewPresenter.Views
-{
-    public interface IRectangulo
-    {
-        string TextoLargo { get; set; }
-        string TextoAncho { get; set; }
-       string TextoArea { get; set; }
-    }
-}
