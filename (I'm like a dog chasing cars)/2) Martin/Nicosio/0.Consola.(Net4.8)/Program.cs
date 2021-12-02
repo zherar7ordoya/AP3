64f6ba981b9959ef21290ae24b8315954f5325a8
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-12-02 13:11:20 -0300

    .

---
+++ (I'm like a dog chasing cars)/2) Martin/Nicosio/0.Consola.(Net4.8)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0.Consola._Net4._8_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
