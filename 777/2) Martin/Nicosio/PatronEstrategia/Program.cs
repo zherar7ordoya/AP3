b1178de89a7cbe8f427ceecc5ce79893a948e22c
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-12-03 12:58:32 -0300

    .

--- 777/2) Martin/Nicosio/PatronEstrategia/Program.cs
+++ 777/2) Martin/Nicosio/PatronEstrategia/Program.cs
@@ -1,53 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
+﻿using static System.Console;
 
-namespace PatronEstrategia
+string dato = string.Empty, opcion = string.Empty;
+double x = 0, y = 0, r = 0;
+
+while (true)
 {
-    class Program
+    WriteLine("1) Suma || 2) Resta || 3) Multplicación || 4) División || 5) Salir");
+    opcion = ReadLine();
+    if (opcion.Equals("5")) { break; }
+
+    WriteLine("Valor de A:");
+    dato = ReadLine();
+    x = Convert.ToDouble(dato);
+
+    WriteLine("Valor de B:");
+    dato = ReadLine();
+    y = Convert.ToDouble(dato);
+
+    IOperacion operacion = opcion switch
     {
-        static void Main(string[] args)
-        {
-            string dato = string.Empty, opcion = string.Empty;
-            double x = 0, y = 0, r = 0;
+        "1" => operacion = new CSuma(),
+        "2" => operacion = new CResta(),
+        "3" => operacion = new CMultiplicacion(),
+        "4" => operacion = new CDivision(),
+        _ => throw new global::System.NotImplementedException()
+    };
+    r = operacion.Operacion(x, y);
+    WriteLine($"Resultado: {r}");
+}
 
-            IOperacion miOperacion = new CSuma();
 
-            while(true)
-            {
-                WriteLine("1) Suma \t 2) Resta \t 3) Multplicación \t 4) División \t 5) Salir");
-                opcion = ReadLine();
-                if (opcion.Equals("5")) { break; }
+#region CLASES E INTERFACES
+internal interface IOperacion
+{
+    double Operacion(double a, double b);
+}
 
-                WriteLine("Valor de A:");
-                dato = ReadLine();
-                x = Convert.ToDouble(dato);
-
-                WriteLine("Valor de B:");
-                dato = ReadLine();
-                y = Convert.ToDouble(dato);
-
-                switch (opcion)
-                {
-                    case "1":
-                        miOperacion = new CSuma();
-                        break;
-                    case "2":
-                        miOperacion = new CResta();
-                        break;
-                    case "3":
-                        miOperacion = new CMultiplicacion();
-                        break;
-                    case "4":
-                        miOperacion = new CDivision();
-                        break;
-                }
-                r = miOperacion.Operacion(x, y);
-                WriteLine($"Resultado: {r}");
-            }
-        }
+class CSuma : IOperacion
+{
+    public double Operacion(double a, double b)
+    {
+        return a + b;
     }
 }
+
+class CResta : IOperacion
+{
+    public double Operacion(double a, double b)
+    {
+        return a - b;
+    }
+}
+
+class CMultiplicacion : IOperacion
+{
+    public double Operacion(double a, double b)
+    {
+        return a * b;
+    }
+}
+
+class CDivision : IOperacion
+{
+    public double Operacion(double a, double b)
+    {
+        return a / b;
+    }
+}
+#endregion
