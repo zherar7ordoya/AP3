dc8043d41f72a36bf260d688277e355b52a18d97
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-12-28 19:58:34 -0300

    .

---
+++ 777/2) Nicosio/PatronFachada/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using Subsistemas;
+
+
+
+
+
+namespace Subsistemas
+{
+    using System;
+    using System.Console;
+    class CSubsistemaCompra
+    {
+
+    }
+    class CSubsistemaAlmacen
+    {
+
+    }
+    class CSubsistemaEnvio
+    {
+        Console.WriteLine("¿Qué?");
+        //ForegroundColor=ConsoleColor.Cyan;
+    }
+}
