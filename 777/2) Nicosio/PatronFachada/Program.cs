57bb44b89aef0667605cc961c75cfc617c3249a5
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-12-29 13:11:00 -0300

    .

--- 777/2) Nicosio/PatronFachada/Program.cs
+++ 777/2) Nicosio/PatronFachada/Program.cs
@@ -1,25 +1,114 @@
-﻿using static System.Console;
+﻿// See https://aka.ms/new-console-template for more information
+
+using static System.Console;
 using Subsistemas;
 
+WriteLine("Hello, World!");
 
+Fachada fachada = new();
+
+fachada.Compra();
+ForegroundColor = ConsoleColor.White;
+WriteLine("<*********************>");
+
+fachada.Compra();
+ForegroundColor = ConsoleColor.White;
+WriteLine("<*********************>");
+
+fachada.Compra();
+ForegroundColor = ConsoleColor.White;
+WriteLine("<*********************>");
 
 
 
 namespace Subsistemas
 {
-    using System;
-    using System.Console;
-    class CSubsistemaCompra
-    {
 
-    }
-    class CSubsistemaAlmacen
+    class Fachada
     {
+        private SubsistemaAlmacen almacen = new();
+        private SubsistemaCompras compras = new();
+        private SubsistemaEnvios envios = new();
 
+        public void Compra()
+        {
+            if(compras.Compra())
+            {
+                if (almacen.SacaAlmacen())
+                {
+                    envios.EnviaPedido();
+                }
+            }
+        }
     }
-    class CSubsistemaEnvio
+
+
+    class SubsistemaCompras
     {
-        Console.WriteLine("¿Qué?");
-        //ForegroundColor=ConsoleColor.Cyan;
+        public bool Compra()
+        {
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("¿Número de tarjeta?");
+            string dato = Console.ReadLine();
+
+            if (dato.Equals("123"))
+            {
+                ForegroundColor = ConsoleColor.Green;
+                WriteLine("Pago aceptado");
+                return true;
+            }
+            else
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("Pago rechazado");
+                return false;
+            }
+        }
     }
+
+
+
+
+    class SubsistemaAlmacen
+    {
+        private int cantidad;
+
+        public SubsistemaAlmacen()
+        {
+            cantidad = 2;
+        }
+
+        public bool SacaAlmacen()
+        {
+            if (cantidad > 0)
+            {
+                ForegroundColor = ConsoleColor.Yellow;
+                WriteLine("Puede enviarse");
+                cantidad--;
+                return true;
+            }
+            else
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("No disponible");
+                return false;
+            }
+        }
+    }
+
+
+
+
+    class SubsistemaEnvios
+    {
+        public void EnviaPedido()
+        {
+            ForegroundColor = ConsoleColor.Cyan;
+            WriteLine("En camino");
+        }
+    }
+
+
 }
+
+
