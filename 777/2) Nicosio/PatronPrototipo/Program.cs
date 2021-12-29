be752298f92b20f8b18a3b9cc9e57dcb3bb8e235
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-12-29 20:09:08 -0300

    .

---
+++ 777/2) Nicosio/PatronPrototipo/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+
+
+
+
+
+
+interface IPrototipo
+{
+    object Clona();
+}
+
+class Valores : IPrototipo
+{
+    private double sumatoria;
+    private int m = 1;
+
+    public double Sumatoria { get => sumatoria; set => sumatoria = value; }
+    public int M { get => m; set => m = value; }
+
+    public Valores()
+    {
+
+    }
+    public Valores(int m)
+    {
+        this.m = m;
+        double sumatoria = 0;
+        for (int n=0; n < 90; n++)
+        {
+            sumatoria += Math.Sin(n * 0.0175);
+        }
+    }
+
+    public override string ToString()
+    {
+        return string.Format($"{sumatoria * m}");
+    }
+
+    public object Clona()
+    {
+        Valores clon = new();
+        clon.M = this.M;
+        clon.Sumatoria = this.Sumatoria;
+        return clon;
+    }
+}
