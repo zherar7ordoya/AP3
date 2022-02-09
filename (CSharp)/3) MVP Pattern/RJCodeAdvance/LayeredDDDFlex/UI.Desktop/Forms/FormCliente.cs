931b587990244a118db74908e17a658df896f595
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-02-09 13:35:55 -0300

    .

---
+++ (CSharp)/3) MVP Pattern/RJCodeAdvance/LayeredDDDFlex/UI.Desktop/Forms/FormCliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UI.Desktop.ApplicationController;
+
+namespace UI.Desktop.Forms
+{
+    public partial class FormCliente : Form
+    {
+        private ClientController client;
+
+        public FormCliente()
+        {
+            InitializeComponent();
+            client = new ClientController();
+        }
+
+        private void FormCliente_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = client.GetClientsAll("");
+        }
+
+        private void txtFiltrar_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = client.GetClientsAll(txtFiltrar.Text);
+        }
+    }
+}
