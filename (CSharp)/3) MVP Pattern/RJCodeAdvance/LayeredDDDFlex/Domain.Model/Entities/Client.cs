f47482376fcc4864f3ed813a268fa70b02b74b3e
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-02-10 12:23:44 -0300

    .

--- (CSharp)/3) MVP Pattern/RJCodeAdvance/LayeredDDDFlex/Domain.Model/Entities/Client.cs
+++ (CSharp)/3) MVP Pattern/RJCodeAdvance/LayeredDDDFlex/Domain.Model/Entities/Client.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Model.Entities
+﻿namespace Domain.Model.Entities
 {
     public class Client
     {
--- (CSharp)/3) MVP Pattern/RJCodeAdvance/LayeredDDDFlex/UI.Desktop/ViewModel/ClientViewModel.cs
+++ (CSharp)/3) MVP Pattern/RJCodeAdvance/LayeredDDDFlex/UI.Desktop/ViewModel/ClientViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UI.Desktop.ViewModel
+﻿namespace UI.Desktop.ViewModel
 {
     public class ClientViewModel
     {
